e7fcb4426506eaea58a967b4a295fbaf42093403
Tim Lehr <tim.lehr@filmakademie.de>
2019-06-13 21:13:09 +0200

    DemoGafferExtension : added TaskAlgo implementation
    To allow for easier tracking of external dependencies in a Gaffer
    script, I added the "TaskAlgo" submodule to the "GafferDemoExtension"
    package. It has multiple functions to retrieve upstream file
    dependencies, based on the node values of the "fileName" plug.
    Additional plug names can be specified in the designated plug name set,
    should the need arise. The file names are returned as strings in a
    Python Set. If needed, the user can also retrieve a set with
    substituted variables for a given context.

--- python/DemoGafferExtension/__init__.py
+++ python/DemoGafferExtension/__init__.py
@@ -3,4 +3,6 @@ __import__( "GafferScene" )
 
 from _DemoGafferExtension import *
 
+import TaskAlgo
+
 __import__( "IECore" ).loadConfig( "GAFFER_STARTUP_PATHS", subdirectory = "DemoGafferExtension" )
